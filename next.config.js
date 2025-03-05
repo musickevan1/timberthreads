958d5f33dff73ec7e77c5342010017b88f9a4f9a
Evan Musick <musickevan1@gmail.com>
2025-03-04 22:42:05 -0600

    Update next.config.js to allow Cloudinary images in Image component

--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   images: {
     unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/dziunzep2/image/upload/**',
+      },
+    ],
   },
   env: {
     NEXT_PUBLIC_ADMIN_PASSWORD: 'timberandthreads2024'
